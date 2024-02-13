d8efe3948d88a912254ca85905358fad027f124c
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2024-02-13 13:26:34 +0100

    Generate client

--- generate-client.sh
+++ generate-client.sh
@@ -3,3 +3,5 @@
 SPEC_PATH=lib/openapi
 
 for i in $(ls $SPEC_PATH/*.yaml); do openapi-generator-cli generate -i $i  -g python -o ./lib/geoserver; done
+
+# yq eval-all '. as $item ireduce ({}; . * $item)' geoserver-rest-openapi/openapi/1.0.0/*.yaml > compiled.yaml\n
